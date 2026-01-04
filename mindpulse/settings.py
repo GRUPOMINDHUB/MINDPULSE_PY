971f2a9d01ebe6e95af0fad0d0377245f97df138
Mindpulse Developer <dev@mindpulse.com>
2026-01-03 23:20:40 -0300

    fix: Ajusta configuracoes para servidor local funcionar corretamente

--- mindpulse/settings.py
+++ mindpulse/settings.py
@@ -16,7 +16,8 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-dev-key-change-in-pro
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
+# Permite acesso local em qualquer interface de rede
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,0.0.0.0', cast=Csv())
 
 # Application definition
 INSTALLED_APPS = [
